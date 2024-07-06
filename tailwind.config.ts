e0b0f4399a398558313b5173a3e1d9f720c46a24
Obed Effum <obedeffum10@gmail.com>
2024-07-06 12:29:52 +0000

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '576px',    // Small screens, mobile portrait
+        'md': '768px',    // Medium screens, tablets
+        'lg': '992px',    // Large screens, desktops
+        'xl': '1200px',   // Extra large screens, wide desktops
+        '2xl': '1440px',  // 2XL screens, 4k and above
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
